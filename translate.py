4a14346c9e3ab40af15a023a93c6d3bf87bc020e
anthem53 <dlwlgus0231@naver.com>
2022-04-04 18:42:43 +0900

    make method to find chrome driver to library

--- translate.py
+++ translate.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import pyperclip
-import chromedriver_autoinstaller as AutoChrome
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 # https://answer-id.com/ko/52791220 셀레니움 창 위치 관련 링크
 driver = None
@@ -37,15 +37,12 @@ def init_translate():
     # 옵션 생성
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    
     path = os.path.dirname(os.path.realpath(__file__))+"//chromedriver.exe"  
     global driver
 
-    chrome_ver      = AutoChrome.get_chrome_version()
-    print(chrome_ver)
-    print("os.path.dirname(os.path.realpath(__file__))",os.path.dirname(os.path.realpath(__file__)))
-    
-    driver = webdriver.Chrome(path, options=options)
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
     driver.set_window_position(0,-2000)
     
     openURL("https://papago.naver.com/")
